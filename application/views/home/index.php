0cd5db3f2f34627e74490b45dbc79b766e3c8b90
Yogi Nugraha <yoginugraha43@gmail.com>
2019-12-19 09:35:48 +0700

    fix conflict pada index

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -44,175 +44,79 @@
   </a>
 </div>
 
+<!-- Marketing Best Saller -->
+<div class="container padto text-center">
+  <strong>HOT ITEMS</strong>
+  <div class="row autoplay">
 
-
-<<<<<<< HEAD <div class="modal fade" id="modalLogin" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h5 class="modal-title  w-100 font-weight-bold" id="exampleModalLabel">Form Login</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+    <div class="col-lg-4">
+      <img src="<?= base_url(); ?>assets/images/Karambit.jpg" class="rounded img-thumbnail" alt="">
+      <div class="card-header">
+        <h5>Ruby Karambit</h5>
       </div>
-      <div class="modal-body">
-        <form class="user">
-          <div class="form-group">
-            <input type="text" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-          </div>
-          <div class="form-group">
-            <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-          </div>
-          <div class="form-group">
-            <div class="custom-control custom-checkbox small">
-              <input type="checkbox" class="custom-control-input" id="customCheck">
-              <label class="custom-control-label" for="customCheck">Remember Me</label>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa aspernatur magnam nobis inventore unde quis harum fuga quasi officiis, beatae ducimus illo, laborum perferendis alias ipsa. Aliquam pariatur rerum officia.</p>
+      <p>
+        <a class="btn btn-info" href="<?= base_url(); ?>detail" role="button">View Detail</a>
+      </p>
+    </div>
+
+    <div class="col-lg-4">
+      <img src="<?= base_url(); ?>assets/images/Karambit-Case-Hardened.jpg" class="rounded img-thumbnail" alt="">
+      <div class="card-header">
+        <h5>Case Hardened Karambit</h5>
+      </div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa aspernatur magnam nobis inventore unde quis harum fuga quasi officiis, beatae ducimus illo, laborum perferendis alias ipsa. Aliquam pariatur rerum officia.</p>
+      <p>
+        <a class="btn btn-info" href="<?= base_url('home/detail'); ?>" role="button">View Detail</a>
+      </p>
+    </div>
+
+    <div class="col-lg-4">
+      <img src="<?= base_url(); ?>assets/images/Karambit-Doppler-Blackpearl.jpg" class="rounded img-thumbnail" alt="">
+      <div class="card-header">
+        <h5>Doppler Blackpearl Karambit</h5>
+      </div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa aspernatur magnam nobis inventore unde quis harum fuga quasi officiis, beatae ducimus illo, laborum perferendis alias ipsa. Aliquam pariatur rerum officia.</p>
+      <p>
+        <a class="btn btn-info" href="<?= base_url('home/detail'); ?>" role="button">View Detail</a>
+      </p>
+    </div>
+
+    <div class="col-lg-4">
+      <img src="<?= base_url(); ?>assets/images/Karambit-Gamma-Doppler.jpg" class="rounded img-thumbnail" alt="">
+      <div class="card-header">
+        <h5>Gamma Doppler Karambit</h5>
+      </div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa aspernatur magnam nobis inventore unde quis harum fuga quasi officiis, beatae ducimus illo, laborum perferendis alias ipsa. Aliquam pariatur rerum officia.</p>
+      <p>
+        <a class="btn btn-info" href="<?= base_url('home/detail'); ?>" role="button">View Detail</a>
+      </p>
+    </div>
+
+  </div>
+  <hr>
+</div>
+
+
+<!-- Tampilan Daftar Barang -->
+<div class="container">
+  <strong>New Arrival</strong>
+  <div class="row row-cols-1 row-cols-md-3">
+
+    <?php $i = 0 ?>
+    <?php foreach ($barang as $brg) : ?>
+      <?php if ($i < 4) : ?>
+        <a href="<?= base_url(); ?>home/detail/<?= $brg['id_barang']; ?>" class="col mb-4">
+          <div class="card h-100">
+            <img src="assets/images/<?= $brg['gambar']; ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title"><?= $brg['nama_barang']; ?></h5>
+              <p class="card-text"><?= $brg['deskripsi'];  ?></p>
             </div>
           </div>
-          <a href="" class="btn btn-primary btn-user btn-block">
-            Login
-          </a>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <div class="text-center">
-          <a class="small" href="forgot-password.html">Forgot Password?</a>
-        </div>
-        <div class="text-center">
-          <<<<<<< HEAD <a href="#modalRegister" class="small" data-toggle="modal">Create an Account!</a>
-        </div>
-        =======
-        <a href="#modalRegister" class="small" data-toggle="modal">Create an Account!</a>
-      </div>
-    </div>
-    >>>>>>> 8e145c7bf321fc4f2453f32ebfb198eb2ca4bcac
+        </a>
+        <?php ++$i; ?>
+      <?php endif; ?>
+    <?php endforeach; ?>
   </div>
-  </div>
-  </div>
-  </div>
-  <!-- end modal pop up -->
-  =======
-  >>>>>>> cca157a0ed9122b87f900347dc4ab1e6af4cbace
-
-  <<<<<<< HEAD <!-- modal pop up register -->
-    <div class="modal fade" id="modalRegister" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header text-center">
-            <h5 class="modal-title  w-100 font-weight-bold" id="exampleModalLabel">Form Register</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form class="user">
-              <div class="form-group row">
-                <div class="col-sm-6 mb-3 mb-sm-0">
-                  <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
-                </div>
-                <div class="col-sm-6">
-                  <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name">
-                </div>
-              </div>
-              <div class="form-group">
-                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
-              </div>
-              <div class="form-group row">
-                <div class="col-sm-6 mb-3 mb-sm-0">
-                  <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                </div>
-                <div class="col-sm-6">
-                  <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <a href="login.html" class="btn btn-primary btn-user btn-block">
-              Register Account
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--  end modal pop up register -->
-    =======
-    >>>>>>> 8e145c7bf321fc4f2453f32ebfb198eb2ca4bcac
-
-    <!-- Marketing Best Saller -->
-    <div class="container padto text-center">
-      <strong>HOT ITEMS</strong>
-      <div class="row autoplay">
-
-        <div class="col-lg-4">
-          <img src="<?= base_url(); ?>assets/images/Karambit.jpg" class="rounded img-thumbnail" alt="">
-          <div class="card-header">
-            <h5>Ruby Karambit</h5>
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa aspernatur magnam nobis inventore unde quis harum fuga quasi officiis, beatae ducimus illo, laborum perferendis alias ipsa. Aliquam pariatur rerum officia.</p>
-          <p>
-            <a class="btn btn-info" href="<?= base_url(); ?>detail" role="button">View Detail</a>
-          </p>
-        </div>
-
-        <div class="col-lg-4">
-          <img src="<?= base_url(); ?>assets/images/Karambit-Case-Hardened.jpg" class="rounded img-thumbnail" alt="">
-          <div class="card-header">
-            <h5>Case Hardened Karambit</h5>
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa aspernatur magnam nobis inventore unde quis harum fuga quasi officiis, beatae ducimus illo, laborum perferendis alias ipsa. Aliquam pariatur rerum officia.</p>
-          <p>
-            <a class="btn btn-info" href="<?= base_url('home/detail'); ?>" role="button">View Detail</a>
-          </p>
-        </div>
-
-        <div class="col-lg-4">
-          <img src="<?= base_url(); ?>assets/images/Karambit-Doppler-Blackpearl.jpg" class="rounded img-thumbnail" alt="">
-          <div class="card-header">
-            <h5>Doppler Blackpearl Karambit</h5>
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa aspernatur magnam nobis inventore unde quis harum fuga quasi officiis, beatae ducimus illo, laborum perferendis alias ipsa. Aliquam pariatur rerum officia.</p>
-          <p>
-            <a class="btn btn-info" href="<?= base_url('home/detail'); ?>" role="button">View Detail</a>
-          </p>
-        </div>
-
-        <div class="col-lg-4">
-          <img src="<?= base_url(); ?>assets/images/Karambit-Gamma-Doppler.jpg" class="rounded img-thumbnail" alt="">
-          <div class="card-header">
-            <h5>Gamma Doppler Karambit</h5>
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa aspernatur magnam nobis inventore unde quis harum fuga quasi officiis, beatae ducimus illo, laborum perferendis alias ipsa. Aliquam pariatur rerum officia.</p>
-          <p>
-            <a class="btn btn-info" href="<?= base_url('home/detail'); ?>" role="button">View Detail</a>
-          </p>
-        </div>
-
-      </div>
-      <hr>
-    </div>
-
-
-    <!-- Tampilan Daftar Barang -->
-    <div class="container">
-      <strong>New Arrival</strong>
-      <div class="row row-cols-1 row-cols-md-3">
-
-        <?php $i = 0 ?>
-        <?php foreach ($barang as $brg) : ?>
-          <?php if ($i < 4) : ?>
-            <a href="<?= base_url(); ?>home/detail/<?= $brg['id_barang']; ?>" class="col mb-4">
-              <div class="card h-100">
-                <img src="assets/images/<?= $brg['gambar']; ?>" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title"><?= $brg['nama_barang']; ?></h5>
-                  <p class="card-text"><?= $brg['deskripsi'];  ?></p>
-                </div>
-              </div>
-            </a>
-            <?php ++$i; ?>
-          <?php endif; ?>
-        <?php endforeach; ?>
-        >>>>>>> 8e145c7bf321fc4f2453f32ebfb198eb2ca4bcac
-      </div>
-    </div>
+</div>
